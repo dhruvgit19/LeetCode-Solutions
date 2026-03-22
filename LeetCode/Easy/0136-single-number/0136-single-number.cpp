9767f1488ffd3b4cf6cb632b2ea3e4be6de800c8
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-03-22 20:19:50 +0530

    Easy 0136-single-number

--- LeetCode/Easy/0136-single-number/0136-single-number.cpp
+++ LeetCode/Easy/0136-single-number/0136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        int unique = 0;
+        int unique=0;
         for(int i=0;i<n;i++){
             unique=unique^nums[i];
         }
