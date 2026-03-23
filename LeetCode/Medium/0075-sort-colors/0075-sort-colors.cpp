4eb7cd9843237b2e93e1c62610b722e85d534ee0
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-03-23 18:31:44 +0530

    Medium 0075-sort-colors

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.cpp
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+
+        // DUTCH NATIONAL FLAG APPROACH : 
+
+        int n = nums.size();
+        int low = 0, mid = 0;
+        int high = n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
