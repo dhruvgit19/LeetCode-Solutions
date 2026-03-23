5ab4d9e4d197c22ade8fe256bc3f7da2f4739247
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-03-23 19:09:33 +0530

    Easy 0169-majority-element

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        // MOORE'S VOTING ALGORITHM
+
+        int count=0; int candidate=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(count==0){
+                candidate = nums[i];
+            }
+            if(nums[i]==candidate) count++;
+            else count--;
+        }
+        return candidate;
+    }
+};
