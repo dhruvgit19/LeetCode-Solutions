1b9f20a5ba38aa436008b1da0ab3d2c1518f03a1
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-03-26 16:31:37 +0530

    Medium 0031-next-permutation

--- LeetCode/Medium/0031-next-permutation/0031-next-permutation.cpp
+++ LeetCode/Medium/0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int idx = -1;
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                idx = i;
+        int ind=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind=i;
                 break;
             }
         }
-
-        if (idx == -1) {
-            reverse(nums.begin(), nums.end());
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
             return;
         }
-
-        for (int i = n - 1; i > idx; i--) {
-            if (nums[i] > nums[idx]) {
-                swap(nums[i], nums[idx]);
+        for(int i=n-1;i>ind;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
                 break;
             }
         }
-
-        reverse(nums.begin() + idx + 1, nums.end());
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
