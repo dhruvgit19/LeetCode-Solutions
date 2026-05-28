aa887dbcc5731e3a76fcdf399d3ee7edf5a69d93
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-05-28 13:01:44 +0400

    Medium 0018-4sum

--- LeetCode/Medium/0018-4sum/0018-4sum.cpp
+++ LeetCode/Medium/0018-4sum/0018-4sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        set<vector<int>> st;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+                    if(sum==target){   
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-
-                set<long long> hashset;
-
-                for(int k = j + 1; k < n; k++) {
-
-                    long long sum =
-                        (long long)nums[i] + nums[j] + nums[k];
-
-                    long long fourth = target - sum;
-
-                    if(hashset.find(fourth) != hashset.end()) {
-
-                        vector<int> temp = {
-                            nums[i],
-                            nums[j],
-                            nums[k],
-                            (int)fourth
-                        };
-
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
                     }
-
-                    hashset.insert(nums[k]);
+                    else if(sum<target) k++;
+                    else l--;
                 }
             }
         }
-
-        return vector<vector<int>>(st.begin(), st.end());
+        return ans;
     }
 };
