9f73a57bbb56c195fc4b0ee0f7fbb37beec8109f
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-06-15 20:50:03 +0400

    Medium 0074-search-a-2d-matrix

--- LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        int low = 0, high = m * n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            int row = mid / n;
-            int col = mid % n;
-
-            int value = matrix[row][col];
-
-            if (value == target)
-                return true;
-            else if (value < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low = 0;
+        int high = (n*m)-1;
+        while (low<=high){
+            int mid = low + (high-low)/2;
+            int row = mid/m;
+            int column = mid%m;
+            if(matrix[row][column] == target) return true;
+            else if(matrix[row][column] < target){
+                low = mid+1;
+            }
+            else high = mid-1;
         }
-
         return false;
     }
 };
