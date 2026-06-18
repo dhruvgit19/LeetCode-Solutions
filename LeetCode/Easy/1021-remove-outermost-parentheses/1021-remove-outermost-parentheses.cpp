1dc4df750866bbd173cd69190de001f22bc66c34
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-06-18 15:33:21 +0400

    Easy 1021-remove-outermost-parentheses

---
+++ LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.size();
+        string ans="";
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(s[i] == '('){
+                if(count>0){
+                    ans.push_back(s[i]);
+                }
+                count++;
+            }
+            else if(s[i] == ')'){
+                count--;
+                if(count>0){
+                    ans.push_back(s[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
