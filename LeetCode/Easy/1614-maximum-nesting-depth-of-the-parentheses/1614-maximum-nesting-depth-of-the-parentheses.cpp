eccd0e7f10e64182f05ca562a5b7c19058daa36e
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-06-19 18:39:27 +0400

    Easy 1614-maximum-nesting-depth-of-the-parentheses

--- LeetCode/Easy/1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ LeetCode/Easy/1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int depth = 0, maxDepth = 0;
-
-        for (char c : s) {
-            if (c == '(') {
-                depth++;
-                maxDepth = max(maxDepth, depth);
+        int n=s.size();
+        int count=0;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                count++;
             }
-            else if (c == ')') {
-                depth--;
+            else if(s[i]==')'){
+                count--;
             }
+            maxi=max(maxi,count);
         }
-
-        return maxDepth;
+        return maxi;
     }
 };
