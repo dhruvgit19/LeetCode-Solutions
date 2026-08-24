96ada100d35c038daea3fa8c1e4da84eb1f0032e
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-08-24 19:21:35 +0530

    Medium 2104-sum-of-subarray-ranges

---
+++ LeetCode/Medium/2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n=nums.size();
+        long long range=0;
+        for(int i=0;i<n;i++){
+            int mini = nums[i];
+            int maxi = nums[i];
+            for(int j=i;j<n;j++){
+                mini = min(mini,nums[j]);
+                maxi = max(maxi,nums[j]);
+                range = range + (maxi-mini);
+            }
+        }
+        return range;
+    }
+};
