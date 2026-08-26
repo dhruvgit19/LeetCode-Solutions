2abcd65c72b3ca20de57660eecd1f9ceaf8107cc
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-08-26 23:35:15 +0530

    Medium 0901-online-stock-span

---
+++ LeetCode/Medium/0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,19 @@
+class StockSpanner {
+private:
+    std::stack<std::pair<int, int>> st; // pair: {price, span}
+
+public:
+    StockSpanner() {}
+
+    int next(int price) {
+        int span = 1;
+
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+
+        st.push({price, span});
+        return span;
+    }
+};
