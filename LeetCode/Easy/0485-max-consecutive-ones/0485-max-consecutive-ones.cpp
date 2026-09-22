b4ce0b1f1bccc595ae7f0f6f7ee4b4cdccda8ecf
Dhruv Mishra <mdhruv.1909@gmail.com>
2026-09-22 22:34:44 +0530

    Easy 0485-max-consecutive-ones

--- LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,13 +5,9 @@ public:
         int count=0;
         int maxcount=0;
         for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                count++;
-                maxcount=max(maxcount,count);
-            }
-            else{
-                count=0;
-            }
+            if(nums[i]==1) count++;
+            else count=0;
+            maxcount=max(maxcount,count);
         }
         return maxcount;
     }
